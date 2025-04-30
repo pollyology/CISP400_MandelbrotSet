ff21af6c2bf657cadb685da7b697c8114cb2ea07
pollyology <pcagaanan01@gmail.com>
2025-04-30 14:32:36 -0700

    Update ComplexPlane.h

--- code/ComplexPlane.h
+++ code/ComplexPlane.h
@@ -8,8 +8,9 @@
 #include <SFML/Graphics/Text.hpp>
 #include <SFML/Graphics/RenderTarget.hpp>
 #include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics.hpp>
 
-using namespace sf; 
+using namespace sf;
 using namespace std;
 
 const unsigned int MAX_ITER = 64;
@@ -23,37 +24,37 @@ enum class State
 	DISPLAYING,
 };
 
-class ComplexPlane
+class ComplexPlane : public Drawable
 {
-	public:
-		ComplexPlane(int pixelWidth, int pixelHeight);
-		void draw(RenderTarget& target, RenderStates states) const;
-		void zoomIn();
-		void zoomOut();
-		void setCenter(Vector2i mousePixel);
-		void setMouseLocation(Vector2i mousePixel);
-		void loadText(Text& text);
-		void updateRender();
+public:
+	ComplexPlane(int pixelWidth, int pixelHeight);
+	void draw(RenderTarget& target, RenderStates states) const;
+	void zoomIn();
+	void zoomOut();
+	void setCenter(Vector2i mousePixel);
+	void setMouseLocation(Vector2i mousePixel);
+	void loadText(Text& text);
+	void updateRender();
 
-	private:
-		// Objects
-		VertexArray m_vArray;
-		State m_state;
-		
-		// Floats
-		Vector2f m_mouseLocation;
-		Vector2f m_plane_center;
-		Vector2f m_plane_size;
-		float m_aspectRatio;
+private:
+	// Objects
+	VertexArray m_vArray;
+	State m_state;
 
-		// Ints
-		Vector2i m_pixel_size; 
-		int m_zoomCount;
+	// Floats
+	Vector2f m_mouseLocation;
+	Vector2f m_plane_center;
+	Vector2f m_plane_size;
+	float m_aspectRatio;
 
-		// Methods
-		int countIterations(Vector2f coord);
-		void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
-		Vector2f mapPixelToCoords(Vector2i mousePixel);
+	// Ints
+	Vector2i m_pixel_size;
+	int m_zoomCount;
+
+	// Methods
+	int countIterations(Vector2f coord);
+	void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
+	Vector2f mapPixelToCoords(Vector2i mousePixel);
 
 };
 
