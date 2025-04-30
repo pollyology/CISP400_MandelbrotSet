fc4cb4b1307ebbe220460dc850bc3e624979254a
pollyology <pcagaanan01@gmail.com>
2025-04-30 14:32:21 -0700

    Update ComplexPlane.cpp

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -1,15 +1,17 @@
 #include "ComplexPlane.h"
 
-ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight) :
-	m_pixel_size{ pixelWidth, pixelHeight },
-	m_plane_center{ 0, 0 },
-	m_plane_size{ BASE_WIDTH, BASE_HEIGHT * m_aspectRatio },
-	m_zoomCount(0),
-	m_state(State::CALCULATING)
+ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
 {
+	m_pixel_size = { pixelWidth, pixelHeight };
 	m_aspectRatio = float(pixelHeight) / float(pixelWidth);
+
+	m_plane_center = { 0, 0 };
+	m_plane_size = { BASE_WIDTH, BASE_HEIGHT * m_aspectRatio };
+	m_zoomCount = 0;
+	m_state = State::CALCULATING;
+
 	m_vArray.setPrimitiveType(Points);
-	m_vArray.resize(size_t(pixelWidth) * size_t(pixelHeight));
+	m_vArray.resize(pixelWidth * pixelHeight);
 }
 
 void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
@@ -19,28 +21,27 @@ void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
 
 void ComplexPlane::updateRender()
 {
-	Uint8 r, g, b;
 	int pixelWidth = m_pixel_size.x;
 	int pixelHeight = m_pixel_size.y;
 
 	if (m_state == State::CALCULATING)
 	{
-		for (int x = 0; x < pixelWidth; x++)
+		for (int j = 0; j < pixelWidth; j++)
 		{
-			for (int y = 0; y < pixelHeight; y++)
+			for (int i = 0; i < pixelHeight; i++)
 			{
-				int newIndex = x + y * pixelWidth;
-				m_vArray[newIndex].position = { (float)x, (float)y };
-				Vector2f newCoord = mapPixelToCoords(Vector2i(x, y));
+				m_vArray[j + i * pixelWidth].position = { (float)j, (float)i };
+				Vector2f newCoord = mapPixelToCoords(Vector2i(j, i));
 				int numIter = countIterations(newCoord);
-				
+
+				Uint8 r, g, b;
 				iterationsToRGB(numIter, r, g, b);
-				m_vArray[newIndex].color = { r, g, b };
+				m_vArray[j + i * pixelWidth].color = { r, g, b };
 			}
-			
+
 		}
+		m_state = State::DISPLAYING;
 	}
-	m_state = State::DISPLAYING;
 }
 
 void ComplexPlane::zoomIn()
@@ -48,6 +49,7 @@ void ComplexPlane::zoomIn()
 	m_zoomCount++;
 	float newX = float(BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount));
 	float newY = float(BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount));
+
 	m_plane_size = Vector2f(newX, newY);
 	m_state = State::CALCULATING;
 }
@@ -57,6 +59,7 @@ void ComplexPlane::zoomOut()
 	m_zoomCount--;
 	float newX = float(BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount));
 	float newY = float(BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount));
+
 	m_plane_size = Vector2f(newX, newY);
 	m_state = State::CALCULATING;
 }
@@ -78,8 +81,8 @@ void ComplexPlane::loadText(Text& text)
 
 	// Assume m_plane_center is center and m_mouseLocation is the cursor
 	ss << "Mandelbrot Set\n";
-	ss << "Center: " << m_plane_center.x << "," << m_plane_center.y << ")\n";
-	ss << "Cursor: " << m_mouseLocation.x << "," << m_mouseLocation.y << ")\n";
+	ss << "Center: (" << m_plane_center.x << "," << m_plane_center.y << ")\n";
+	ss << "Cursor: (" << m_mouseLocation.x << "," << m_mouseLocation.y << ")\n";
 	ss << "Left-click to Zoom in\n";
 	ss << "Right-click to Zoom out\n";
 
@@ -91,14 +94,14 @@ int ComplexPlane::countIterations(Vector2f coord)
 	// Count the number of iterations of the set for the given coordinate as specified above
 	// Mandelbrot Set Equation: f(z) = z^2 + c
 
-	complex<double> z = 0;
-	complex<double> c = {coord.x, coord.y}; // complex = real + imaginary, ex: 2 + 3i
+	complex<float> c = { coord.x, coord.y }; // complex = real + imaginary, ex: 2 + 3i
+	complex<float> z = c;
 
-	int iterations = 0;
+	size_t iterations = 0;
 
-	while (abs(z) < 2.0 && iterations <= MAX_ITER) // max magnitude is 2.0
+	while (abs(z) < 2.0 && iterations < MAX_ITER) // max magnitude is 2.0
 	{
-		z = pow(z, 2) + c;
+		z = z * z + c;
 		iterations++;
 	}
 
@@ -108,30 +111,21 @@ int ComplexPlane::countIterations(Vector2f coord)
 void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
 {
 	// TODO: Rlly bad color palette, change later - 4/28 PC
-	if (count == MAX_ITER)
+	if (count >= MAX_ITER)
 	{
 		r = g = b = 0;
 	}
-	else if (count > 0 && count <= MAX_ITER * 0.25)
-	{
-		r = g = b = 256 * 0.25;
-	}
-	else if (count > 16 && count <= MAX_ITER * 0.50)
-	{
-		r = g = b = 256 * 0.50;
-	}
-	else if (count > 32 && count <= MAX_ITER * 0.75)
-	{
-		r = g = b = 256 * 0.75;
-	}
 	else
 	{
-		r = 218;
-		g = 247;
-		b = 166;
-	}
+		// Simple RGB gradient: fades from blue to white
+		Uint8 brightness = static_cast<Uint8>(255 * count / MAX_ITER);
 
+		r = brightness;
+		g = brightness;
+		b = 255; // Full blue base
+	}
 }
+
 
 Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel)
 {
@@ -142,22 +136,9 @@ Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel)
 	// d - c = m_plane_size.x/y 
 	// +c = (m_plane_center.x/y  - m_plane_size.x/y)
 	// ((mousePixel.x - m_plane_size.x) / (m_pixel_size.x - m_plane_size.x)) * m_plane_size.x + (m_plane_center.x - m_plane.size.x / 2.0)
-	
-	int xMousePos = mousePixel.x;
-	int yMousePos = mousePixel.y;
 
-	float xPlaneWidth = m_plane_size.x;
-	float yPlaneHeight = m_plane_size.y;
-
-	float xPlaneCenter = m_plane_center.x;
-	float yPlaneCenter = m_plane_center.y;
-
-	float xScreenWidth = m_pixel_size.x;
-	float yScreenHeight = m_pixel_size.y;
-
-
-	float xCoord = ((xMousePos - xPlaneWidth) / (xScreenWidth - xPlaneWidth)) * xPlaneWidth + (xPlaneCenter - xPlaneWidth / 2.0);
-	float yCoord = ((yMousePos - yPlaneHeight) / (yScreenHeight - yPlaneHeight)) * yPlaneHeight + (yPlaneCenter - yPlaneHeight / 2.0);;
+	float xCoord = (float(mousePixel.x - 0) / (m_pixel_size.x - 0)) * m_plane_size.x + (m_plane_center.x - m_plane_size.x / 2.0);
+	float yCoord = (float(mousePixel.y - m_pixel_size.y) / (0 - m_pixel_size.y)) * m_plane_size.y + (m_plane_center.y - m_plane_size.y / 2.0);
 
 	return Vector2f(xCoord, yCoord);
 }
